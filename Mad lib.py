00c39f70462fecbcb5bd6052d5bcde5b0157c062
Tyson Vorwaller <tysonvorwaller@gmail.com>
2018-09-07 13:25:56 -0600

    chapter1 code exe

---
+++ Mad lib.py
@@ -0,0 +1,12 @@
+#Tyson Vorwaller periods 4&5
+#Mad Lib
+#Blank fill ins
+word1 = "pencils."
+word2 = "sniff new cars."
+word3 = "life"
+word4 = "run for my life"
+word5 = "megaladon"
+word6= "Dr. Phil"
+#Story
+print("My favorite food is ", word1, "And I like to ", word2, "I wish I had a ", word3 + ".", "And on the weekends I", word4 + "." + "I have a pet ", word5, "named", word6 + ".")
+input()
